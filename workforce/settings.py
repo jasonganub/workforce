2dda81a9671e4edaf0087a0e3d75601e30d910c7
Jason Ganub <jasonganub@gmail.com>
2017-09-26 15:13:57 -0700

    Included django rest framework.

--- workforce/settings.py
+++ workforce/settings.py
@@ -45,7 +45,17 @@ INSTALLED_APPS = (
     'applicant',
     'experience',
     'job',
+    # external libraries
+    'rest_framework',
 )
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
