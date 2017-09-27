e3d42a4747349ffff3b862c55d43ed7d83d422bd
Jason Ganub <jasonganub@gmail.com>
2017-09-26 20:42:17 -0700

    Got GET, PUT, and POST working.

--- job/views.py
+++ job/views.py
@@ -1,29 +1,34 @@
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from rest_framework.parsers import JSONParser
 from .models import Job
 from .serializers import JobSerializer
 
 
+@csrf_exempt
 def job_detail(request, job_id):
     """
     View or update a job.
     """
-    if request.method == 'GET':
-        try:
-            job = Job.objects.get(pk=job_id)
-            serializer = JobSerializer(job)
-            return JsonResponse(serializer.data)
-        except Job.DoesNotExist:
-            return HttpResponse(status=404)
+    try:
+        job = Job.objects.get(pk=job_id)
+    except Job.DoesNotExist:
+        return HttpResponse(status=404)
 
-    elif request.method == 'POST':
-        pass
+    if request.method == 'GET':
+        serializer = JobSerializer(job)
+        return JsonResponse(serializer.data)
 
     elif request.method == 'PUT':
-        pass
+        data = JSONParser().parse(request)
+        serializer = JobSerializer(job, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
 
-
+@csrf_exempt
 def create_job(request):
     """
     Create a job.
