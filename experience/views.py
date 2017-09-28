cf5342c303162c92bff7ba24ef079b8778f8821d
Jason Ganub <jasonganub@gmail.com>
2017-09-27 19:55:20 -0700

    Create experience urls and view for POST.

--- experience/views.py
+++ experience/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from rest_framework.parsers import JSONParser
+from .models import Experience
+from .serializers import ExperienceSerializer
 
-# Create your views here.
+
+@csrf_exempt
+def create_experience(request):
+    """
+    Create an experience.
+    """
+    data = JSONParser().parse(request)
+    serializer = ExperienceSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
--- workforce/urls.py
+++ workforce/urls.py
@@ -7,5 +7,6 @@ from applicant import urls
 urlpatterns = [
     url(r'^jobs/', include('job.urls')),
     url(r'^applicants/', include('applicant.urls')),
+    url(r'^experience/', include('experience.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
